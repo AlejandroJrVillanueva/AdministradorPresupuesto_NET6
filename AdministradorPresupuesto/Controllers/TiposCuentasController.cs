57a97ff07e7547203d5ad2a89ba28ccc4ae0bce0
Alejandro jr <alejandrovillanuevareina@gmail.com>
2022-05-17 17:39:54 -0300

    Se agrega Formulario TiposCuentas con su Modelo y su Controlador.  Se deshabilita manejar tipos de datos null en configuracion del project

---
+++ AdministradorPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,19 @@
+﻿using AdministradorPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdministradorPresupuesto.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            return View();
+        }
+    }
+}
