02a878bb092de4341b4c2994af5e205233641cab
Alejandro jr <alejandrovillanuevareina@gmail.com>
2022-05-17 20:02:32 -0300

    Validando el formulario TiposCuentas en back(tag-helpers), front(jquery) y validacion personalizada por Attribute

--- AdministradorPresupuesto/Controllers/TiposCuentasController.cs
+++ AdministradorPresupuesto/Controllers/TiposCuentasController.cs
@@ -13,6 +13,10 @@ namespace AdministradorPresupuesto.Controllers
         [HttpPost]
         public IActionResult Crear(TipoCuenta tipoCuenta)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
             return View();
         }
     }
--- AdministradorPresupuesto/Models/TipoCuenta.cs
+++ AdministradorPresupuesto/Models/TipoCuenta.cs
@@ -1,8 +1,15 @@
-﻿namespace AdministradorPresupuesto.Models
+﻿using AdministradorPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdministradorPresupuesto.Models
 {
     public class TipoCuenta
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 4,
+            ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
