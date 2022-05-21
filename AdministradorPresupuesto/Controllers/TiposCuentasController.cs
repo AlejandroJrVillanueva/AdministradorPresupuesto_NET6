a0c1af20027db179eb57f9247ce931681f433243
Alejandro jr <alejandrovillanuevareina@gmail.com>
2022-05-20 21:41:22 -0300

    Insertando Un TiposCuentas en la BD (Patron Repository)

--- AdministradorPresupuesto/Controllers/TiposCuentasController.cs
+++ AdministradorPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,10 +1,20 @@
 ﻿using AdministradorPresupuesto.Models;
+using AdministradorPresupuesto.Servicio;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdministradorPresupuesto.Controllers
 {
     public class TiposCuentasController: Controller
     {
+        private readonly ITiposCuentasRepository _tiposCuentasRepository;
+
+        public TiposCuentasController(ITiposCuentasRepository tiposCuentasRepository)
+        {
+            _tiposCuentasRepository = tiposCuentasRepository;   
+        }
+
+        public ITiposCuentasRepository TiposCuentasRepository { get; }
+
         [HttpGet]
         public IActionResult Crear()
         {
@@ -17,6 +27,9 @@ namespace AdministradorPresupuesto.Controllers
             {
                 return View(tipoCuenta);
             }
+
+            tipoCuenta.UsuarioId = 1;
+            _tiposCuentasRepository.Crear(tipoCuenta);
             return View();
         }
     }
---
+++ AdministradorPresupuesto/Servicio/TiposCuentasRepository.cs
@@ -0,0 +1,29 @@
+﻿using AdministradorPresupuesto.Models;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace AdministradorPresupuesto.Servicio
+{
+    public interface ITiposCuentasRepository
+    {
+        void Crear(TipoCuentaViewModel tipoCuentaViewModel);
+    }
+    public class TiposCuentasRepository : ITiposCuentasRepository
+    {
+        private readonly string connectionString;
+        public TiposCuentasRepository(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DesarrolloConnection");
+        }
+
+        public void Crear(TipoCuentaViewModel tipoCuentaViewModel)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = connection.QuerySingle<int>($@"INSERT INTO TiposCuentas(Nombre,UsuarioId,Orden)
+                VALUES (@Nombre,@UsuarioId,0); 
+                SELECT SCOPE_IDENTITY();", tipoCuentaViewModel);
+
+            tipoCuentaViewModel.Id = id;
+        }
+    }
+}
